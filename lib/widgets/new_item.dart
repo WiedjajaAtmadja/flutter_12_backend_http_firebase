2baabbcf730bf788ffbc4fded03d864b6f3a3a04
Wiedjaja Atmadja <swiedjaja@binus.ac.id>
2023-06-05 19:10:37 +0700

    Step 1. Initial Code

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,10 +1,8 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import '../data/categories.dart';
-import '../models/category.dart';
-// import '../models/grocery_item.dart';
+
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -24,23 +22,14 @@ class _NewItemState extends State<NewItem> {
   void _saveItem() {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-      final url = Uri.https(
-          'ce-binus-iot-course-default-rtdb.asia-southeast1.firebasedatabase.app',
-          'shopping-list.json');
-      http.post(
-        url,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: json.encode(
-          {
-            'name': _enteredName,
-            'quantity': _enteredQuantity,
-            'category': _selectedCategory.title,
-          },
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: DateTime.now().toString(),
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory,
         ),
       );
-      // Navigator.of(context).pop();
     }
   }
 
